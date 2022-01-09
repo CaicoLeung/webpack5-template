02b92d468fa401e1467710474b50cadbc3e7237f
CaicoLeung <lkk1053190902@gmail.com>
2022-01-09 22:49:55 +0800

    feat: react root

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import ImgLazyPage from "@/pages/img-lazy/index";
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <ImgLazyPage />
+    </div>
+  )
+}
+
+export default App;
