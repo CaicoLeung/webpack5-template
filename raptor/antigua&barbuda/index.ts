fefa621e66efd3ce17fe4b1a4f3258a0ad58f848
CaicoLeung <caico.liang@imanagesystems.com>
2022-02-10 18:56:13 +0800

    feat: raptor

---
+++ raptor/antigua&barbuda/index.ts
@@ -0,0 +1,13 @@
+// import "@/assets/css/raptor_core.css";
+// import "@/assets/css/raptor_foot.css";
+// import "@/assets/css/raptor_slick.css";
+// import "@/assets/css/raptor_branch.css";
+
+// import "./css/raptor_core.css";
+// import "./css/raptor_foot.css";
+// import "./css/raptor_slick.css";
+// import "./css/raptor_branch.css";
+
+import "./css/raptor_build.css";
+
+console.log("Test Caico");
---
+++ raptor/dubai/index.ts
@@ -0,0 +1,8 @@
+// import "@/assets/css/raptor_core.css";
+// import "@/assets/css/raptor_foot.css";
+// import "@/assets/css/raptor_slick.css";
+// import "@/assets/css/raptor_branch.css";
+
+import "./css/raptor_build.css";
+
+console.log("Test Caico");
