284d6fb497d1bd9f339826458a7d0b13d122bec6
CaicoLeung <caico.liang@imanagesystems.com>
2022-02-11 17:12:08 +0800

    fix: 代码优化

--- raptor/barbados/index.ts
+++ raptor/barbados/index.ts
@@ -1,13 +1,6 @@
-// import "@/assets/css/raptor_core.css";
-// import "@/assets/css/raptor_foot.css";
-// import "@/assets/css/raptor_slick.css";
-// import "@/assets/css/raptor_branch.css";
-
 // import "./css/raptor_core.css";
 // import "./css/raptor_foot.css";
 // import "./css/raptor_slick.css";
 // import "./css/raptor_branch.css";
 
 import "./css/raptor_build.css";
-
-console.log("Test Caico");
--- raptor/st_lucia/index.ts
+++ raptor/st_lucia/index.ts
@@ -1,8 +1,3 @@
-// import "@/assets/css/raptor_core.css";
-// import "@/assets/css/raptor_foot.css";
-// import "@/assets/css/raptor_slick.css";
-// import "@/assets/css/raptor_branch.css";
-
 // import "./css/raptor_core.css";
 // import "./css/raptor_foot.css";
 // import "./css/raptor_slick.css";
